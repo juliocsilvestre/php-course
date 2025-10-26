18e765663f95216e91aed49091fedc0def17db4e
juliocsilvestre <jcss.silvestre@gmail.com>
2025-10-26 11:40:01 -0300

    first questions

---
+++ 2_conceitos_basicos/4_comentarios/index.php
@@ -0,0 +1,14 @@
+<?php
+
+// este é um comentario
+
+# este também é um comentario
+
+/*
+    este é um comentario
+    de múltiplas linhas
+*/
+
+echo "Olá, Mundo! <br>"; // comentário após uma instrução
+
+?>
---
+++ 2_conceitos_basicos/5_exercicio_2/index.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+Julio, 31 Anos
+Cabelo Preto
+1,70 de altura
+usa oculos
+ama tecnologia
+*/
+
+$pessoa = "Julio";
+$hobbie = "tecnologia";
+$altura = "1.70";
+
+echo "Olá, meu nome é $pessoa, tenho $altura m de altura e amo $hobbie.";
+
+
+?>
